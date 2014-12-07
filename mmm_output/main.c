8024f6369f46afdfb12dbc0b6483f8362ce8a89d
Evan Wallace <onlyone@senet.com.au>
2014-12-07 18:47:37 +1100

    Initial Commit

---
+++ mmm_output/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  mmm_output
+//
+//  Created by Evan Wallace on 7/12/2014.
+//  Copyright (c) 2014 Geko Creations. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
